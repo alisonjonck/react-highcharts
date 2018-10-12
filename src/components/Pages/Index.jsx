d83a2e4c83060a20b196d3127837a97353b45720
Alison Henrique Jonck <alisonjonck@gmail.com>
2018-10-12 16:41:01 -0300

    add poc ColumnChart loading

--- src/components/Pages/Index.jsx
+++ src/components/Pages/Index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from '../Atoms/Card.jsx';
 import HelloWorld from '../Atoms/HelloWorld.jsx';
+import ColumnChart from '../Mols/ColumnChart.jsx';
 
 const Index = () => {
     return (
         <Card>
             <HelloWorld />
+            <ColumnChart />
         </Card>
     );
 }
